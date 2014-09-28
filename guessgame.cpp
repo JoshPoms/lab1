f51c48f94628215dc72b2e86f274a865be7c1165
JoshPoms <pomerenkjt30@uww.edu>
2014-09-28 13:07:46 -0500

    initial commit

---
+++ guessgame.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+using namespace std;
+//Josh Pomerenke        guess the number
+
+
+int main()
+{
+        //set the number
+        srand(time(NULL));
+        int num=rand() % 10;
+        //get number from user
+        int guess;
+        cout<<"Guess the number"<<endl;
+        cin>>guess;
+
+        if(guess==num)
+        {
+                cout<<"You guessed that shit right!";<<endl;
+        }else{
+                cout<<"YOU FOOL!";<<endl;
+        }
+        return(0);
+
+}
